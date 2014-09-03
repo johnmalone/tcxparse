6ffbb760c8677db06fe5ec556342a25c073d86bc
John Malone <johnm04@gmail.com>
2014-09-03 19:09:45 +0100

    tweak map settings

--- public/js/maps.js
+++ public/js/maps.js
@@ -4,12 +4,12 @@ var plotlist;
 var plotlayers=[];
 function initmap(id) {
 	// set up the map
-	map[id] = new L.Map('map_'+id, null, { zoomControl:false });
+	map[id] = new L.Map('map_'+id, {dragging: false, scrollWheelZoom: false, doubleClickZoom: false, touchZoom: false, boxZoom: false, zoomControl: false});
 
 	// create the tile layer with correct attribution
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+	var osm = new L.TileLayer(osmUrl, { attribution: osmAttrib});		
 
 	// start the map in South-East England
 	
